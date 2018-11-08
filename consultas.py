841c00a9d994e9f8dd6edf5153467f82f38d52a6
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2018-11-08 15:53:04 +0100

    Add files via upload

--- consultas.py
+++ consultas.py
@@ -1,60 +1,65 @@
 # -*- coding: utf-8 -*-
- 
+
 ##
 ## Carlos Bilbao, Mario Bocos, Álvaro López y David Élbez declaramos que esta soluci´on es fruto exclusivamente
 ## de nuestro trabajo personal. No hemos sido ayudados por ninguna otra persona ni hemos
 ## obtenido la soluci´on de fuentes externas, y tampoco hemos compartido nuestra soluci´on
 ## con nadie. Declaramos adem´as que no hemos realizado de manera deshonesta ninguna otra
 ## actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los dem´as.
-## 
-
+##
 import pymongo
 from pymongo import MongoClient
-from bottle import get, route, run, template
+from bottle import get, route, run, template, request
 
 # Conectamos nuestro servidor web al servidor de Mongo
 mongoclient = MongoClient()
 
-# Cogemos la colección de la BBDD 
+# Cogemos la colección de la BBDD
 db = mongoclient.giw
 collection = db['usuarios']
 
 @get('/find_users')
 def find_users():
-	# Todas las combinaciones de name, surname y/o birthdate
-    # http://localhost:8080/find_users?name=Luz
-    # http://localhost:8080/find_users?name=Luz&surname=Romero	
-    # http://localhost:8080/find_users?name=Luz&surname=Romero&birthdate=2006-08-14
+    	# Todas las combinaciones de name, surname y/o birthdate
+    	# http://localhost:8080/find_users?name=Luz
+	# http://localhost:8080/find_users?name=Luz&surname=Romero
+        # http://localhost:8080/find_users?name=Luz&surname=Romero&birthdate=2006-08-14
 	name = "NULL"
 	surname = "NULL"
 	birthdate = "NULL"
-	name = request.headers.get('name')
+	name = request.headers.get("/find_users?", headers={"content-type":"name"})
+	surname = request.headers.get('surname')
 	print(name)
 	#result = collection.find({name: 'Pepe', surname: '', birthdate: '1971-02-03'})
 	#print(result)
 
 @get('/find_email_birthdate')
 def email_birthdate():
+	print("hola")
     # http://localhost:8080/find_email_birthdate?from=1973-01-01&to=1990-12-31
 
 
 @get('/find_country_likes_limit_sorted')
 def find_country_likes_limit_sorted():
-    # http://localhost:8080/find_country_likes_limit_sorted?country=Irlanda&likes=movies,animals&limit=4&ord=asc
+     print("hola")
+	# http://localhost:8080/find_country_likes_limit_sorted?country=Irlanda&likes=movies,animals&limit=4&ord=asc
 
 
 @get('/find_birth_month')
 def find_birth_month():
+	 print("hola")
   # http://localhost:8080/find_birth_month?month=abril
 
 
 @get('/find_likes_not_ending')
 def find_likes_not_ending():
+	 print("hola")
   # http://localhost:8080/find_likes_not_ending?ending=s
 
 
-@get('/find_leap_year')     
+@get('/find_leap_year')
 def find_leap_year():
+	 print("hola")
   # http://localhost:8080/find_leap_year?exp=20
 
 
