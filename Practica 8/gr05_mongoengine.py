f94efeaa5245f0645d773aae5e3da7a62759ac7a
Álvaro López García <33002461+KillerKing18@users.noreply.github.com>
2018-11-26 11:57:53 +0100

    Add files via upload

--- Practica 8/gr05_mongoengine.py
+++ Practica 8/gr05_mongoengine.py
@@ -7,7 +7,7 @@ class Usuario(Document):
 	primer_apellido = StringField(required=True)
 	segundo_apellido = StringField()
 	fecha_nacimiento = DateTimeField(required=True)
-	ultimos_accesos = ListField(ComplexDateTimeField())
+	ultimos_accesos = ListField(ComplexDateTimeField()) # Hacer de longitud 10
 	tarjetas_credito = ListField(EmbeddedDocumentField(Tarjeta_Credito))
 	pedidos = ListField(ReferenceField(Pedido, reverse_delete_rule = PULL))
 	
@@ -18,3 +18,20 @@ class Tarjeta_Credito(EmbeddedDocument):
 	anio_caducidad = StringField(required=True, min_length = 2, max_length = 2, regex = "[0-9]")
 	codigo_verificacion = StringField(required=True, min_length = 3, max_length = 3, regex = "[0-9]")
 	
+class Pedido(Document):
+	precio_total = IntField(required=True)
+	fecha = ComplexDateTimeField(required=True)
+	lista_lineas = ListField(EmbeddedDocumentField(Linea_Pedido, required=True))
+	
+class Linea_Pedido(EmbeddedDocument):
+	cantidad = IntField(required=True)
+	precio_producto = IntField(required=True)
+	nombre = StringField(required=True)
+	precio_linea = IntField(required=True)
+	producto = ReferenceField(Producto, required=True)
+
+class Producto(Document):
+	codigo_barras = StringField(required=True, unique=True) # Formato EAN-13
+	nombre = StringField(required=True)
+	categoria_principal = IntField(required=True) # Numero natural
+	categorias_secundarias = ListField(IntField) # Numeros naturales
