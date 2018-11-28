4808bf5832a3c759d0cc1aaba3d5294402ee511d
Álvaro López García <33002461+KillerKing18@users.noreply.github.com>
2018-11-28 10:36:35 +0100

    Add files via upload

--- Practica 8/gr05_mongoengine.py
+++ Practica 8/gr05_mongoengine.py
@@ -30,7 +30,7 @@ class Linea_Pedido(EmbeddedDocument):
 	precio_linea = IntField(required=True)
 	producto = ReferenceField(Producto, required=True)
 
-	def es_valido(self):
+	def clean(self):
 		if(self.cantidad * self.precio_producto != self.precio_linea): # Esquema punto 3
 			raise ValidationError("El precio no coincide con la muliplicacion de cantidades y precio")
 		if(self.nombre != self.producto.nombre): # Esquema punto 4
@@ -38,8 +38,8 @@ class Linea_Pedido(EmbeddedDocument):
 		return True
 
 class Pedido(Document):
-	precio_total = FloatField(min_value=1,required=True)
-	fecha = ComplexDateTimeField(min_value=2018,required=True)
+	precio_total = IntField(min_value=1,required=True)
+	fecha = ComplexDateTimeField(required=True)
 	lista_lineas = ListField(EmbeddedDocumentField(Linea_Pedido), required=True)
 
 	def clean(self):
@@ -64,14 +64,14 @@ class Usuario(Document):
 	pedidos = ListField(ReferenceField(Pedido, reverse_delete_rule = PULL)) # Articulo punto 7 -> reverse_delete_rule
 
 	def clean(self):
-		if(len(self.ultimos_accesos > 10)): # No puede haber mas de 10
+		if(len(self.ultimos_accesos) > 10): # No puede haber mas de 10
 			raise ValidationError("No puede haber mas de 10 ultimos accesos")
 		if(isnotValid(self.DNI)): # La funcion auxiliar comprueba que se cumple el formato del DNI nacional
 			raise ValidationError("El DNI no tiene el formato correcto") # Esquema punto 1
 
 def isnotValid(DNI):
 	num = DNI[:8]
-	num = num % 23
+	num = int(num) % 23
 
 	if num == 0:
 		letra = "T"
@@ -132,22 +132,75 @@ def sumPrec(lista):
 	return sum
 
 def isnotEANValid(ean):
-	# TODO - Codigo Python del Wiki?
 	return False
 
+def insertar():
+	# Usuario 1
+	producto1a = Producto(codigo_barras = "123456789", nombre = "Toalla", categoria_principal = 4, categorias_secundarias=[4, 5, 6])
+	producto1a.save()
+	producto2a = Producto(codigo_barras = "987654321", nombre = "Balon", categoria_principal = 3)
+	producto2a.save()
+	producto3a = Producto(codigo_barras = "ABCDEFGHI", nombre = "Sombrilla", categoria_principal = 1, categorias_secundarias=[1, 2, 3])
+	producto3a.save()
+	producto4a = Producto(codigo_barras = "472958205", nombre = "Helado", categoria_principal = 3, categorias_secundarias=[3, 4, 5])
+	producto4a.save()
+	producto5a = Producto(codigo_barras = "847295837", nombre = "Agua", categoria_principal = 7)
+	producto5a.save()
+	linea_pedido1a = Linea_Pedido(cantidad = 4, precio_producto = 2, nombre = "Toalla", precio_linea = 8, producto = producto1a)
+	linea_pedido2a = Linea_Pedido(cantidad = 10, precio_producto = 5, nombre = "Balon", precio_linea = 50, producto = producto2a)
+	linea_pedido3a = Linea_Pedido(cantidad = 7, precio_producto = 15, nombre = "Sombrilla", precio_linea = 105, producto = producto3a)
+	linea_pedido4a = Linea_Pedido(cantidad = 10, precio_producto = 3, nombre = "Helado", precio_linea = 30, producto = producto4a)
+	linea_pedido5a = Linea_Pedido(cantidad = 10, precio_producto = 1, nombre = "Agua", precio_linea = 10, producto = producto5a)
+	pedido1a = Pedido(precio_total = 163, fecha = "2018,11,27,20,59,40,000000", lista_lineas = [linea_pedido1a, linea_pedido2a, linea_pedido3a])
+	pedido1a.save()
+	pedido2a = Pedido(precio_total = 40, fecha = "2015,3,20,01,45,33,000000", lista_lineas = [linea_pedido4a, linea_pedido5a])
+	pedido2a.save()
+	tarjeta1a = Tarjeta_Credito(nombre = "Juan Ramirez Perez", numero = "1572894830294827", mes_caducidad = "01", anio_caducidad = "22", codigo_verificacion = "012")
+	tarjeta2a = Tarjeta_Credito(nombre = "Juan Ramirez Perez", numero = "4839285720195927", mes_caducidad = "02", anio_caducidad = "19", codigo_verificacion = "123")
+	usuario1 = Usuario(DNI = "50346706M", nombre = "Juan", primer_apellido = "Ramirez", segundo_apellido = "Perez", fecha_nacimiento = ("1996, 04, 22"), ultimos_accesos = ["2018,11,27,19,59,40,000000", "2018,11,27,18,59,40,000000"], tarjetas_credito = [tarjeta1a, tarjeta2a], pedidos = [pedido1a, pedido2a])
+	usuario1.save()
 	
-###### INSERCIONES ######
+	# Usuario 2
+	producto1b = Producto(codigo_barras = "843927583", nombre = "Tarjeta Grafica", categoria_principal = 2)
+	producto1b.save()
+	producto2b = Producto(codigo_barras = "843928573", nombre = "Placa base", categoria_principal = 15, categorias_secundarias=[15, 8, 6])
+	producto2b.save()
+	producto3b = Producto(codigo_barras = "573950392", nombre = "RAM", categoria_principal = 23, categorias_secundarias=[23, 46, 16])
+	producto3b.save()
+	producto4b = Producto(codigo_barras = "859396038", nombre = "Tornillo", categoria_principal = 12)
+	producto4b.save()
+	producto5b = Producto(codigo_barras = "950395732", nombre = "Alicate", categoria_principal = 47, categorias_secundarias=[47, 12])
+	producto5b.save()
+	producto6b = Producto(codigo_barras = "958305937", nombre = "Martillo", categoria_principal = 17)
+	producto6b.save()
+	producto7b = Producto(codigo_barras = "853954739", nombre = "Sierra", categoria_principal = 18, categorias_secundarias=[18, 20])
+	producto7b.save()
+	linea_pedido1b = Linea_Pedido(cantidad = 5, precio_producto = 200, nombre = "Tarjeta Grafica", precio_linea = 1000, producto = producto1b)
+	linea_pedido2b = Linea_Pedido(cantidad = 5, precio_producto = 100, nombre = "Placa base", precio_linea = 500, producto = producto2b)
+	linea_pedido3b = Linea_Pedido(cantidad = 5, precio_producto = 75, nombre = "RAM", precio_linea = 375, producto = producto3b)
+	linea_pedido4b = Linea_Pedido(cantidad = 15, precio_producto = 1, nombre = "Tornillo", precio_linea = 15, producto = producto4b)
+	linea_pedido5b = Linea_Pedido(cantidad = 2, precio_producto = 15, nombre = "Alicate", precio_linea = 30, producto = producto5b)
+	linea_pedido6b = Linea_Pedido(cantidad = 2, precio_producto = 20, nombre = "Martillo", precio_linea = 40, producto = producto6b)
+	linea_pedido7b = Linea_Pedido(cantidad = 1, precio_producto = 45, nombre = "Sierra", precio_linea = 45, producto = producto7b)
+	pedido1b = Pedido(precio_total = 1875, fecha = "2017,03,07,19,35,22,000000", lista_lineas = [linea_pedido1b, linea_pedido2b, linea_pedido3b])
+	pedido1b.save()
+	pedido2b = Pedido(precio_total = 45, fecha = "2016,04,18,19,00,00,000000", lista_lineas = [linea_pedido4b, linea_pedido5b])
+	pedido2b.save()
+	pedido3b = Pedido(precio_total = 85, fecha = "2008,01,22,22,00,00,000000", lista_lineas = [linea_pedido6b, linea_pedido7b])
+	pedido3b.save()
+	tarjeta1b = Tarjeta_Credito(nombre = "Marcos Martinez", numero = "3859284029184938", mes_caducidad = "05", anio_caducidad = "24", codigo_verificacion = "999")
+	tarjeta2b = Tarjeta_Credito(nombre = "Marcos Martinez", numero = "5948372059382942", mes_caducidad = "07", anio_caducidad = "30", codigo_verificacion = "888")
+	tarjeta3b = Tarjeta_Credito(nombre = "Marcos Martinez", numero = "4837295830593850", mes_caducidad = "12", anio_caducidad = "20", codigo_verificacion = "777")
+	usuario2 = Usuario(DNI = "58204938B", nombre = "Marcos", primer_apellido = "Martinez", fecha_nacimiento = ("1996, 04, 22"), tarjetas_credito = [tarjeta1b, tarjeta2b, tarjeta3b], pedidos = [pedido1b, pedido2b, pedido3b])
+	usuario2.save()
 
-producto1 = Producto(codigo_barras = "123456789", nombre = "Toalla", categoria_principal = 4, categorias_secundarias=[4, 5, 6])
-producto1.save()
-linea_pedido1 = Linea_Pedido(cantidad = 4, precio_producto = 2, nombre = "Toalla", precio_linea = 8, producto = producto1)
-if linea_pedido1.es_valido():
-	print("Valido")
-else:
-	print("Invalido")
-# Hacer save sobre EmbeddedDocument no tiene efecto, asi que que pasa con clean
-
-
+	# Usuario 3
+	usuario3 = Usuario(DNI = "84938293Y", nombre = "Pepe", primer_apellido = "Garcia", fecha_nacimiento = ("1965, 02, 15"))
+	usuario3.save()
 
 #TODO 
-#	Formato EAN_13
+#	5
+#	DNI (regex, extranjeros)
+#	Tarjeta regex
+#	Comprobar valor maximo y minimo para el mes de la tarjeta?
+#	Hacer numeros de la tarjeta StringField o IntField?
