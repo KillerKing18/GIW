81dc8c0e7425749b4cd941c54638aba7fbc5111d
Álvaro López García <33002461+KillerKing18@users.noreply.github.com>
2018-11-29 15:49:21 +0100

    Add files via upload

---
+++ Practica 9/consultas.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+ 
+##
+## Carlos Bilbao, Mario Bocos, Álvaro López y David Élbez declaramos que esta solución es fruto exclusivamente
+## de nuestro trabajo personal. No hemos sido ayudados por ninguna otra persona ni hemos
+## obtenido la solución de fuentes externas, y tampoco hemos compartido nuestra solución
+## con nadie. Declaramos además que no hemos realizado de manera deshonesta ninguna otra
+## actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los demás.
+##
+import pymongo
+from pymongo import MongoClient
+from bottle import get, route, run, template, request
+
+# Conectamos nuestro servidor web al servidor de Mongo
+mongoclient = MongoClient()
+
+# Cogemos la colección de la BBDD
+db = mongoclient.giw
+usuarios = db['usuarios']
+pedidos = db['pedidos']
+
+#Pedidos:
+#	_id = ObjectId(hex)
+#	lineas = lista de elementos con precio(FloatField), nombre(StringField), total(FLoatFIeld) #		y cantidad (IntField)
+#	cliente = IntField
+#	total = FloatField
+#Usuarios:
+#	_id = IntFIeld
+#	pais = StringField
+#	edad = IntField
+#	nombre = StringField
+#	apellido1 = StringField
+#	apellido2 = StringField
+
+@get('/top_countries')
+# http://localhost:8080/top_countries?n=3
+def agg1():
+	n = int(request.query.n)
+	result = usuarios.aggregate([
+		{"$group": {"_id": "$pais", "count": {"$sum":1}}},
+		{"$sort": {"count" : -1, "pais" : 1}},
+		{"$limit": n}])
+
+	return template("output", Cursor = result)
+
+
+@get('/products')
+# http://localhost:8080/products?min=2.34
+def agg2():
+	precio = float(request.query.min)
+	result = pedidos.aggregate([
+		{"$unwind": "$lineas"},
+		{"$match": {"lineas.precio" : {"$gte" : precio}}},
+		{"$group": {"_id": "$lineas.nombre", "count": {"$sum":"$lineas.cantidad"}}}
+		])
+	return template("output2", Cursor = result)
+
+    
+@get('/age_range')
+# http://localhost:8080/age_range?min=80
+def agg3():
+    pass
+    
+    
+@get('/avg_lines')
+# http://localhost:8080/avg_lines
+def agg4():
+    pass
+    
+    
+@get('/total_country')
+# http://localhost:8080/total_country?c=Alemania
+def agg5():
+    pass
+    
+        
+if __name__ == "__main__":
+    # No cambiar host ni port ni debug
+    run(host='localhost',port=8080,debug=True)
---
+++ Practica 9/output2.tpl
@@ -0,0 +1,33 @@
+<!--
+Carlos Bilbao, Mario Bocos, Álvaro López y David Élbez declaramos que esta solución es fruto exclusivamente
+de nuestro trabajo personal. No hemos sido ayudados por ninguna otra persona ni hemos
+obtenido la solución de fuentes externas, y tampoco hemos compartido nuestra solución
+con nadie. Declaramos además que no hemos realizado de manera deshonesta ninguna otra
+actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los demás.
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<h1>CONSULTA</h1>
+</head>
+<body>
+
+	<table>
+                <tr>
+                       <th>Nombre de producto</th>
+                       <th>Numero de unidades vendidas</th>
+                       <th>Precio unitario</th>
+                </tr>
+   
+	%count = 0
+	%for elem in Cursor:
+		%count = count + 1
+		<tr>
+		<th style="font-weight:normal;">{{elem['_id']}}</th>
+		<th style="font-weight:normal;" >{{elem['count']}}</th>
+		<tr>
+	%end	
+	</table>
+<h2>Numero de elementos encontrados = {{count}}</h2>
+</body>
+</html>
